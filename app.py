eb02b1a92a3d56ccc8c8e4f6f5dcded74cedb064
melica <melica.mirsafian@thomsonreuters.com>
2020-07-11 21:11:14 -0400

    pass code through POST method, save in json

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+import json
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/your-url')
+
+@app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
-    return render_template('your_url.html', code=request.args['code'])
+    if request.method == 'POST':
+        urls = {}
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home'))
